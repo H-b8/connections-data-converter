08fa8571b59796785716f152fc71c8dcfdc01a05
CL <carolinemv.le@gmail.com>
2021-01-04 20:41:11 -0800

    updates instructions

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,13 @@ const App = () => {
 	const [showFollowing, setShowFollowing] = useState(false);
 	const [showMutuals, setShowMutuals] = useState(false);
 	const [showInvalid, setShowInvalid] = useState(false);
+	const [showInstructions, setShowInstructions] = useState(false);
 
 	const { register, handleSubmit } = useForm();
+
+	const handleShowInstructions = () => {
+		setShowInstructions(true);
+	}
 
 	const handleShowFollowers = () => {
 		setShowFollowing(false);
@@ -31,7 +36,7 @@ const App = () => {
 		setShowFollowing(false);
 		setShowMutuals(true);
 	}
-	
+
 	let mutuals = [];
 	const handleMutual = username => {
 		const mutual = followers.find(element => element === username);
@@ -72,26 +77,43 @@ const App = () => {
 	return (
 		<>
 			<div id="submission">
-				<p id="header-text"><b>STEP 1:</b> Download data // <b>STEP 2:</b> Unzip and open folder with "part_1" at the end // <b>STEP 3:</b> Open the file "connections.json"</p>
 				<header>HELLO WORLD!</header>
 				<form onSubmit={handleSubmit(onSubmit)} style={{ width: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
 					<textarea
 						id="paste"
-						placeholder="STEP 4: COPY+PASTE THE TEXT FROM YOUR CONNECTIONS.JSON FILE HERE"
+						placeholder="PASTE YOUR 'CONNECTIONS.JSON' FILE'S TEXT HERE"
 						name="jsonPaste"
 						ref={register}
 					/>
 					<button id="white-button" type="submit">VIEW CONNECTIONS</button>
 				</form>
-				<div style={{height: '1rem', marginTop: '1rem'}}>
+				<div style={{ height: '1rem', marginTop: '1rem' }}>
 					{showInvalid &&
 						<p>
-							<a href="http://github.com/h-b8/connections-data-converter/" target="_blank" rel="noopener noreferrer" style={{color: 'red'}}>
-								INVALID TEXT, PLEASE TRY AGAIN OR CLICK HERE FOR HELP.
+							<a href="http://github.com/h-b8/connections-data-converter/" target="_blank" rel="noopener noreferrer" style={{ color: 'red' }}>
+								INVALID TEXT, PLEASE TRY AGAIN.
 							</a>
 						</p>
 					}
 				</div>
+				<div onClick={() => handleShowInstructions()} style={{cursor: 'pointer'}}>
+					<div className="help-div">
+						?
+					</div>
+				</div>
+				{showInstructions &&
+					<div className="instructions">
+						1. If you haven't already downloaded your IG data, go to your profile and open <b>Settings</b> and then <b>Security</b>. Under <b>Data and History</b>, click <b>Download Data</b> and follow the instructions<br/><br/>
+						2. When receiving your email confirmation, follow instructions on downloading your data files to your computer<br/><br/>
+						3. Unzip your files<br/><br/>
+						4. Open the folder that has <b>part_1</b> at the end of its name<br/><br/>
+						5. In this folder, open the file named <b>connections.json</b><br/><br/>
+						6. CMD/CTRL+A and COPY the text in this file<br/><br/>
+						7. PASTE text into text area of application<br/><br/>
+						8. Hit <b>VIEW CONNECTIONS</b>, and voila!<br/><br/>
+					</div>
+				}
+
 			</div>
 
 			{hasConnections &&
@@ -110,7 +132,7 @@ const App = () => {
 								{followers.map(user => {
 									return (
 										<a href={`http://instagram.com/${user}`} target="_blank" rel="noopener noreferrer">
-											<p>{user}</p><br/>
+											<p>{user}</p><br />
 										</a>
 									);
 								})}
@@ -137,8 +159,8 @@ const App = () => {
 						</div>
 					</div>
 
-					<div id="footer-div">
-						<a href="http://github.com/h-b8/" id="footer-text" target="_blank" rel="noopener noreferrer">
+					<div className="footer-div">
+						<a href="http://github.com/h-b8/" className="footer-text" target="_blank" rel="noopener noreferrer">
 							<p>MADE BY BAITES</p>
 						</a>
 					</div>
